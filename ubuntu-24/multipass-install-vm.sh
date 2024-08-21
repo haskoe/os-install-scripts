007e5b83eb48a4e5d58a8ef2d0184bba1ea6439d
hgeas <henrik@haskoe.dk>
2024-08-21 13:59:23 +0200

    multipass ssh key

--- ubuntu-24/multipass-install-vm.sh
+++ ubuntu-24/multipass-install-vm.sh
@@ -5,5 +5,5 @@ multipass purge
 multipass launch $VM_TYPE -c 2 -m 2G -d 20G -n $VM_NAME
 multipass exec $VM_NAME  -- bash -c "sudo apt update && sudo apt -y dist-upgrade && sudo apt install -y git freerdp2-x11 openconnect openvpn && mkdir -p ~/dev/haskoe && cd ~/dev/haskoe && git clone https://github.com/haskoe/os-install-scripts.git && sudo shutdown -h now"
 multipass start $VM_NAME
-multipass exec $VM_NAME  -- bash -c "echo `cat ~/.ssh/id_${HOSTNAME}.pub` >> ~/.ssh/authorized_keys"
-cat ~/.ssh/id_${HOSTNAME}.pub | multipass exec $VM_NAME -- tee -a .ssh/authorized_keys
+multipass exec $VM_NAME  -- bash -c "echo `cat ~/.ssh/id_ed25519.pub` >> ~/.ssh/authorized_keys"
+cat ~/.ssh/id_ed25519.pub | multipass exec $VM_NAME -- tee -a .ssh/authorized_keys
