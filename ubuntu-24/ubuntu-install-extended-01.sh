133794962b5c7c91b757bc608f3753fecdcd25ff
haskoe <henrik@haskoe.dk>
2024-11-23 14:48:21 +0100

    zabbly

--- ubuntu-24/ubuntu-install-extended-01.sh
+++ ubuntu-24/ubuntu-install-extended-01.sh
@@ -14,3 +14,20 @@ sudo apt install libharfbuzz-dev libfribidi-dev
 
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VER}/quarto-${QUARTO_VER}-linux-amd64.deb
 sudo gdebi quarto-${QUARTO_VER}-linux-amd64.deb
+
+# zabbly
+sudo mkdir -p /etc/apt/keyrings/
+sudo wget -q https://pkgs.zabbly.com/key.asc -O /etc/apt/keyrings/zabbly.asc
+sudo sh -c 'cat <<EOF > /etc/apt/sources.list.d/zabbly-kernel-stable.sources
+Enabled: yes
+Types: deb
+URIs: https://pkgs.zabbly.com/kernel/stable
+Suites: $(. /etc/os-release && echo ${VERSION_CODENAME})
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/zabbly.asc
+
+EOF'
+
+sudo apt -y update
+sudo apt install -y linux-zabbly
