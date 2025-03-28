d56abf7314f05257fb866f5688f9fb603949e80f
heas <henrik@haskoe.dk>
2025-03-28 13:15:38 +0100

    nerd fonts

--- arch/2025/02.install-auto.sh
+++ arch/2025/02.install-auto.sh
@@ -7,7 +7,7 @@ sudo localectl set-locale LANG=${PREFERRED_LOCALE}
 # yay
 sudo pacman --noconfirm -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si 
 
-yay -S base-devel dracut gnupg pass bash-completion jq mplayer vlc hunspell-da hunspell-en ttc-iosevka task goreleaser-bin meld lazygit zellij broot sublime-merge rustdesk-bin fzf thunar gvfs keychain fwupd less 7zip smplayer  base-devel gnupg pass bash-completion jq thunar tmux fzf terminator wezterm zstd 7zip inotify-tools ffmpeg keychain less gvfs fwupd inotify-tools rdesktop pavucontrol udev-block-notify dunst thunderbird inotify-tools tk rsync #pragmatapro-fonts pass
+yay -S base-devel dracut gnupg pass bash-completion jq mplayer vlc hunspell-da hunspell-en ttc-iosevka task goreleaser-bin meld lazygit zellij broot sublime-merge rustdesk-bin fzf thunar gvfs keychain fwupd less 7zip smplayer  base-devel gnupg pass bash-completion jq thunar tmux fzf terminator wezterm zstd 7zip inotify-tools ffmpeg keychain less gvfs fwupd inotify-tools rdesktop pavucontrol udev-block-notify dunst thunderbird inotify-tools tk rsync ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono #pragmatapro-fonts pass
 
 # erlang/elixir stuff
 yay -S inets erlang-inets erlang-os_mon erlang-runtime_tools erlang-ssl erlang-xmerl erlang-parsetools
